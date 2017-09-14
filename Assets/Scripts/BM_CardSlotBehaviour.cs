6a8ccdf3758e9695e0bbeb5ae87d17dbc43e3497
Liam Wilson <liamryanwilson@hotmail.co.uk>
2017-09-14 16:41:52 +0100

    Added working betting machine with a card slot.

---
+++ Assets/Scripts/BM_CardSlotBehaviour.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BM_CardSlotBehaviour : MonoBehaviour
+{
+    [SerializeField] private float pulseTime = 1.0f;
+    private BettingMachineBehaviour bm;
+    private Light cardLight;
+    private float maxLightIntensity, pulseChange;
+
+    private void Start()
+    {
+        bm = GetComponentInParent<BettingMachineBehaviour>();
+        cardLight = GetComponent<Light>();
+
+        maxLightIntensity = cardLight.intensity;
+        pulseChange = maxLightIntensity / pulseTime;
+        cardLight.intensity = 0.0f;
+    }
+
+    float pulseTimer = 0.0f;
+    private void Update()
+    {
+        if (!bm.insertedCard)
+            Pulse(Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CardBehaviour cb = other.GetComponent<CardBehaviour>();
+        if (cb && !bm.insertedCard)
+            CardInserted(cb);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (bm.insertedCard && other.gameObject == bm.insertedCard.gameObject)
+            CardRemoved();
+    }
+
+    private void CardInserted(CardBehaviour cb)
+    {
+        cb.transform.position = transform.position;
+        cb.transform.rotation = transform.rotation;
+        cb.GetComponent<Rigidbody>().isKinematic = true;
+
+        pulseTimer = 0.0f;
+        cardLight.intensity = maxLightIntensity;
+        if (pulseChange > 0)
+            pulseChange *= -1.0f;
+
+        bm.insertedCard = cb;
+        bm.ToggleOn();
+    }
+
+    private void CardRemoved()
+    {
+        bm.insertedCard.GetComponent<Rigidbody>().isKinematic = false;
+        bm.insertedCard = null;
+        bm.ToggleOn();
+    }
+
+    private void Pulse(float deltaTime)
+    {
+        pulseTimer += deltaTime;
+        cardLight.intensity += pulseChange * deltaTime;
+
+        if (pulseTimer > pulseTime)
+        {
+            pulseTimer = 0.0f;
+            pulseChange *= -1.0f;
+        }
+    }
+}
---
+++ Assets/Scripts/BettingMachineBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BettingMachineBehaviour : MonoBehaviour
+{
+    [HideInInspector] public CardBehaviour insertedCard;
+    [SerializeField] private Material screenBaseMaterial;
+    [SerializeField] private GameObject[] screens;
+    [SerializeField] private Camera[] feeds;
+
+    private RenderTexture[] screenTextures;
+    private Material[] screenMaterials;
+    private bool isOn = false;
+
+    private void Start()
+    {
+        screenTextures = new RenderTexture[screens.Length];
+        screenMaterials = new Material[screens.Length];
+
+        for (int i = 0; i < screens.Length; i++) {
+            screenTextures[i] = new RenderTexture(256, 256, 24);
+
+            screenMaterials[i] = new Material(screenBaseMaterial);
+            screenMaterials[i].mainTexture = screenTextures[i];
+            screenMaterials[i].color = isOn ? Color.white : Color.black;
+
+            screens[i].GetComponent<MeshRenderer>().material = screenMaterials[i];
+
+            SetScreenFeed(feeds[i], i);
+        }
+    }
+
+    public void SetScreenFeed(Camera feed, int screenIndex)
+    {
+        if (!feed)
+            return;
+
+        if (screenIndex >= 0 && screenIndex < screens.Length)
+            feed.targetTexture = screenTextures[screenIndex];
+    }
+
+    public void ToggleOn()
+    {
+        isOn = !isOn;
+
+        for (int i = 0; i < screens.Length; i++)
+            screenMaterials[i].color = isOn ? Color.white : Color.black;
+    }
+
+    private void OnValidate()
+    {
+        if (feeds.Length != screens.Length) {
+            Camera[] feedsTemp = feeds;
+
+            feeds = new Camera[screens.Length];
+            for (int i = 0; i < feeds.Length; i++)
+                feeds[i] = feedsTemp[i];
+        }
+    }
+}
